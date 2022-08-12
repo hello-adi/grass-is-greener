845168ab83cb98fd79d22c5a777f19147e8d1d85
Aditya Lakshminarayanan <aditya.net09@gmail.com>
2022-08-12 23:41:04 +0530

    added boilerplate for location to coordinate

---
+++ grass.py
@@ -0,0 +1,15 @@
+import os
+import requests
+
+API_KEY = ""
+
+
+def location_coordinate(loc):
+    # api call for getting cordinate
+    pass
+
+
+location = input("Enter the location:")
+location_lat_long = location_coordinate(location)
+
+# api call to openweather
